d4e16a1b41662676e942c7104cbcef5999f5c024
Fritz <shadowfritz2020@gmail.com>
2026-03-09 22:30:15 -0500

    Java LSP

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -20,7 +20,7 @@ vim.keymap.set("n", "yy", "+yy", { noremap = true, silent = true})
 
 vim.keymap.set("n", "ZE", "<cmd>q!<CR>", { noremap = true, silent = true })
 
---Rust clippy additio
+--LSP Bullshit
 
 vim.lsp.config("rust_analyzer", {
   settings = {
@@ -30,6 +30,16 @@ vim.lsp.config("rust_analyzer", {
   },
 })
 
+-- Set Java 21 for Neovim LSPs
+vim.env.JAVA_HOME = "/usr/lib/jvm/java-21-openjdk"
+vim.env.PATH = vim.env.JAVA_HOME .. "/bin:" .. vim.env.PATH
+lsp.enable("jdtls")
+
+lsp.config("jdtls", {
+  cmd = { "jdtls", "-data", vim.fn.expand("~/.cache/jdtls/workspace") },
+  root_dir = vim.fn.getcwd(), -- forces current folder as project root
+  filetypes = { "java" },
+})
 vim.lsp.enable("rust_analyzer")
 vim.opt.autochdir = true
 
--- lua/dan/lazy/theme.lua
+++ lua/dan/lazy/theme.lua
@@ -6,7 +6,7 @@ return
       config = function()
 
   		vim.g.edge_style = "aura" -- "aura" for Dark
- 	    vim.g.edge_transparent_background = 1 -- Optional
+ 	    vim.g.edge_transparent_background = 0 -- Optional
         -- Optionally configure and load the colorscheme
         -- directly inside the plugin declaration.
         vim.g.edge_enable_italic = true
